d4d3ca392e754b373ea0edfbe0d5c37a2d0f3b61
Jared Searle <jjsearle@usf.edu>
2021-01-19 13:38:00 -0500

    Add project files.

---
+++ JaredSearleHelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaredSearleHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause to display Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
